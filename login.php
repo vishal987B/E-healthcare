1a4e6ba5df3dda5b7b3d19d8242580a12a6cf0f0
Ganga Sagar Singh <83035420+vishal987B@users.noreply.github.com>
2021-08-04 23:40:18 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+include 'header.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="pathLogin.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<div class="loginHeader">
+<ul class="nav nav-pills nav-fill">
+<li class="nav-item">
+    <a class="nav-link active " aria-current="page" href="pLogin.php">Patient</a>
+  </li>
+  <li class="nav-item" >
+    <a class="nav-link" href="dLogin.php">Doctor</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="pathLogin.php">Pathology</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="adminLogin.php">Admin</a>
+  </li>
+</ul>
+</div>  
+</body>
+</html>
